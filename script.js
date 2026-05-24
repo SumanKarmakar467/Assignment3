12b9b8f5f8f643781eaf8e449341c31da863713b
SumanKarmakar467 <karmakarsuman12138@gmail.com>
2026-05-24 08:44:07 +0530

    add styling for remove button in the shopping cart

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let buttons= document.querySelectorAll(".Add-items");
+let buttons = document.querySelectorAll(".Add-items");
 let cartBody = document.querySelector("#cart-body");
 let emptyRow = document.querySelector("#empty-row");
 let totalPrice = document.querySelector("#total-price");
@@ -6,37 +6,58 @@ let totalPrice = document.querySelector("#total-price");
 let count = 1;
 let total = 0;
 
-for(let i=0; i<buttons.length; i++){
-    buttons[i].addEventListener("click",() => {
+for (let i = 0; i < buttons.length; i++) {
 
-        if(emptyRow){
-            emptyRow.remove();
-            emptyRow = null ;
-        }
+    let row = null;
+
+    buttons[i].addEventListener("click", () => {
 
         let services = buttons[i].parentElement.querySelector("div").textContent;
 
-        let parts= services.split("-");
+        let parts = services.split("-");
 
         let serviceName = parts[0].trim();
 
-        let price = parts[1].replace("₹","").trim();
+        let price = parts[1].replace("₹", "").trim();
 
-        let row = document.createElement("tr");
+        if (buttons[i].textContent.includes("Add")) {
 
-        row.innerHTML = `
-            <td>${count}</td>
-            <td>${serviceName}</td>
-            <td>${price}</td>
-        `;
+            if(emptyRow) {
+                emptyRow.remove();
+                emptyRow = null;
+            }
+            row = document.createElement("tr");
+         
+            row.innerHTML = `
+                <td>${count}</td>
+                <td>${serviceName}</td>
+                <td>${price}</td>
+            `;
+         
+            cartBody.appendChild(row);
+        
+            count++;
+        
+            total += Number(price);
+        
+            totalPrice.textContent = total;
+        
+            buttons[i].textContent = "Remove";
+            buttons[i].classList.add("remove-btn");
+         
+        }       
+        else {
+            row.remove();
+            
+            count--;
 
-        cartBody.appendChild(row);
+            total -= Number(price);
 
-        count++;
+            totalPrice.textContent = total;
 
-        total += Number(price);
+            buttons[i].textContent = "Add items";
+            buttons[i].classList.remove("remove-btn");
 
-        totalPrice.textContent = total;
-    })
-
+        }
+  });
 }
